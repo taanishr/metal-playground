d67708fb784af648f47e6532a18c5bcfb4253c73
Taanish R <tnreja@gmail.com>
2025-01-12 21:20:55 -0800

    Started texture API

---
+++ MTKTextureBindings/MTKTextureBindings/MTKTextures.swift
@@ -0,0 +1,35 @@
+//
+//  MTKTextures.swift
+//  MTKTextureBindings
+//
+//  Created by Taanish Reja on 1/12/25.
+//
+
+import Foundation
+import Metal
+import MetalKit
+
+public func createMTKTextureLoader(devicePtr: UnsafeMutableRawPointer) -> UnsafeMutableRawPointer {
+    let device = Unmanaged<MTLDevice>.fromOpaque(devicePtr).takeUnretainedValue();
+    let loader = MTKTextureLoader(device: device);
+    let loaderPtr = UnsafeMutableRawPointer(Unmanaged.passRetained(loader).toOpaque());
+    return loaderPtr;
+}
+
+public func releaseMTKTextureLoader(loaderPtr: UnsafeMutableRawPointer) {
+    let loader = Unmanaged<MTKTextureLoader>.fromOpaque(loaderPtr);
+    loader.release();
+}
+
+public func createTexture(loaderPtr: UnsafeMutableRawPointer, filePath: UnsafePointer<CChar>) -> UnsafeMutableRawPointer?
+{
+    let loader = Unmanaged<MTKTextureLoader>.fromOpaque(loaderPtr).takeUnretainedValue();
+    let fileURL = URL(fileURLWithPath: String(cString: filePath));
+    guard let texture = try? loader.newTexture(URL: fileURL)
+    else {
+        return nil;
+    }
+    
+    let texturePtr = UnsafeMutableRawPointer(Unmanaged.passRetained(texture).toOpaque());
+    return texturePtr; // must transfer ownership using NS::SharedPtr in C++
+}
